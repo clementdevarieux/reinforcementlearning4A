58816c1447b3ccbd32ad3eace923b7554387de1d
clementdevarieux <devarieux.clement@gmail.com>
2024-07-02 21:37:08 +0200

    secret env implem

---
+++ src/Env/SecretEnv0.rs
@@ -0,0 +1,27 @@
+use rand::Rng;
+use std::collections::HashMap;
+use rand::distributions::{Distribution, Uniform};
+use std::ffi::c_void;
+
+pub struct SecretEnv0 {
+    pub agent_pos: i32,
+    pub num_states: i32,
+    pub num_actions: i32,
+    pub S: Vec<i32>,
+    pub A: Vec<i32>,
+    pub R: Vec<i32>,
+    pub T: Vec<i32>,
+    pub p: Vec<Vec<Vec<Vec<f32>>>>
+}
+
+impl SecretEnv0 {
+    pub fn init() -> *mut c_void {
+        let secret_env_0_new: libloading::Symbol<unsafe extern fn() -> *mut c_void> =
+            unsafe { LIB.get(b"secret_env_0_new") }.expect("Failed to load function `secret_env_0_new`");
+        unsafe {
+            let env = secret_env_0_new();
+            env
+        }
+    }
+
+}
--- src/Env/mod.rs
+++ src/Env/mod.rs
@@ -1,3 +1,4 @@
 pub mod LineWorld;
 pub mod GridWorld;
 pub mod Shifumi;
+pub mod SecretEnv0;
